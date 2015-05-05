fe1587621bb14db876aa0fcd76c38c6c75cf0959
whitefirer <whitefirer@gmail.com>
2015-05-05 18:08:12 +0800

    Update urlcoding.h

--- DesktopWeather/urlcoding.h
+++ DesktopWeather/urlcoding.h
@@ -7,24 +7,21 @@ using namespace std;
 class strCoding
 {
 public:
-    strCoding(void);
-    ~strCoding(void);
+	strCoding(void);
+	~strCoding(void);
    
-    void UTF_8ToGB2312(string &pOut, char *pText, int pLen);//utf_8转为gb2312
-    void GB2312ToUTF_8(string& pOut,char *pText, int pLen); //gb2312 转utf_8
-    string UrlGB2312(char * str);                           //urlgb2312编码
-    string UrlUTF8(char * str);                             //urlutf8 编码
-    string UrlUTF8Decode(string str);                  //urlutf8解码
-    string UrlGB2312Decode(string str);                //urlgb2312解码
+	void UTF_8ToGB2312(string &pOut, char *pText, int pLen);//utf_8杞负gb2312
+	void GB2312ToUTF_8(string& pOut,char *pText, int pLen); //gb2312 杞瑄tf_8
+	string UrlGB2312(char * str);							//urlgb2312缂栫爜
+	string UrlUTF8(char * str);								//urlutf8 缂栫爜
+	string UrlUTF8Decode(string str);						//urlutf8瑙ｇ爜
+	string UrlGB2312Decode(string str);						//urlgb2312瑙ｇ爜
   
 private:
-    void Gb2312ToUnicode(WCHAR* pOut,char *gbBuffer);
-    void UTF_8ToUnicode(WCHAR* pOut,char *pText);
-    void UnicodeToUTF_8(char* pOut,WCHAR* pText);
-    void UnicodeToGB2312(char* pOut,WCHAR uData);
-    char CharToInt(char ch);
-    char StrToBin(char *str);
+	void Gb2312ToUnicode(WCHAR* pOut,char *gbBuffer);
+	void UTF_8ToUnicode(WCHAR* pOut,char *pText);
+	void UnicodeToUTF_8(char* pOut,WCHAR* pText);
+	void UnicodeToGB2312(char* pOut,WCHAR uData);
+	char CharToInt(char ch);
+	char StrToBin(char *str);
 };
- 
- 
- 
