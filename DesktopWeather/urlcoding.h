3f7981f87e67397d1c0ad8584c8f2b1c393ebfd2
whitefirer <whitefirer@gmail.com>
2015-05-05 18:09:03 +0800

    Update urlcoding.h

--- DesktopWeather/urlcoding.h
+++ DesktopWeather/urlcoding.h
@@ -12,10 +12,10 @@ public:
    
 	void UTF_8ToGB2312(string &pOut, char *pText, int pLen);//utf_8转为gb2312
 	void GB2312ToUTF_8(string& pOut,char *pText, int pLen); //gb2312 转utf_8
-	string UrlGB2312(char * str);							//urlgb2312编码
-	string UrlUTF8(char * str);								//urlutf8 编码
-	string UrlUTF8Decode(string str);						//urlutf8解码
-	string UrlGB2312Decode(string str);						//urlgb2312解码
+	string UrlGB2312(char * str);				//urlgb2312编码
+	string UrlUTF8(char * str);				//urlutf8 编码
+	string UrlUTF8Decode(string str);			//urlutf8解码
+	string UrlGB2312Decode(string str);			//urlgb2312解码
   
 private:
 	void Gb2312ToUnicode(WCHAR* pOut,char *gbBuffer);
